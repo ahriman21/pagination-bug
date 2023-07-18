7332a90a763f55e6712f324106ba82a92dec9197
ahriman <109135045+ahriman21@users.noreply.github.com>
2023-07-18 17:42:32 +0330

    Update custom_tag_function.py

--- custom_tag_function.py
+++ custom_tag_function.py
@@ -4,10 +4,10 @@
 def my_url(value, field_name, urlencode=None):
     url = '?{}={}'.format(field_name, value)
 
-    if unicode:
-      querystring = urlencode.split('&')
-      filtered_querystring = filter(lambda p: p.split('=')[0]=field_name, querystring)
-      encoded_querystring = '&'.join(filtered_querystring)
-      url = '{}&{}'.format(url, encoded_querystring)
+    if urlencode:
+          querystring = urlencode.split('&')
+          filtered_querystring = filter(lambda p: p.split('=')[0]!=field_name, querystring)
+          encoded_querystring = '&'.join(filtered_querystring)
+          url = '{}&{}'.format(url, encoded_querystring)
 
-  return url
+    return url
